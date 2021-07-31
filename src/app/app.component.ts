a45339eebdba814758d65f46c8061688cb92cf46
Joe Romine <joe.romine@ufl.edu>
2021-07-30 22:59:14 -0700

    L1 finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ponyracer';
-  onNewRace(): void{
-alert('Clowns')
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { RacesComponent } from './races/races.component';
+import { Title } from '@angular/platform-browser';
 
 @NgModule({
-  declarations: [
-    AppComponent, 
-    RacesComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+  constructor(title: Title) {
+    title.setTitle('Pony App');
+  }
+}
