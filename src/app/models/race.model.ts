ca883b36a190965f143163f07ef2211ca4522296
Joe Romine <joe.romine@ufl.edu>
2021-08-04 21:45:33 -0700

    8fin

--- src/app/models/race.model.ts
+++ src/app/models/race.model.ts
@@ -1,3 +1,12 @@
 export class RaceModel {
+  id!: number;
   name!: string;
+  ponies!: Array<Ponies>;
+  startInstant!: string;
+}
+
+class Ponies {
+  id!: number;
+  name!: string;
+  color!: string;
 }
--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { RaceModel } from '../models/race.model';
 
 @Component({
   selector: 'pr-race',
   templateUrl: './race.component.html',
   styleUrls: ['./race.component.css']
 })
-export class RaceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class RaceComponent {
+  @Input() raceModel!: RaceModel;
 }
