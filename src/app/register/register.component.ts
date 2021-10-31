f188846b7947b48731e0a27e04fcc12e849c2894
Joe Romine <joe.romine@ufl.edu>
2021-10-30 17:29:49 -0700

    add static method to validate formGroup

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -17,7 +17,7 @@ export class RegisterComponent {
     this.passwordForm = fb.group({
       password: (this.passwordCtrl = fb.control('', Validators.required)),
       confirmPassword: (this.confirmPasswordCtrl = fb.control('', Validators.required))
-    });
+    }, RegisterComponent.passwordMatch);
 
     this.userForm = fb.group({
       login: (this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)])),
@@ -27,4 +27,12 @@ export class RegisterComponent {
   }
 
   register() {}
+
+  static passwordMatch(passwordfg: FormGroup){
+    if(passwordfg.controls['password'].value == passwordfg.controls['confirmPassword'].value){
+      return {matchingError: null}
+    }
+    return {matchingError: true}
+  }
+
 }
