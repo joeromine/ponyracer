931b7640b379dc84150e58cf7a7bf2719f5f5ce0
Joe Romine <joe.romine@ufl.edu>
2021-10-30 17:13:00 -0700

    add confirm password field

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,18 +8,21 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class RegisterComponent {
   userForm: FormGroup;
   passwordCtrl: FormControl;
-  loginCtrl: FormControl = new FormControl('', Validators.required);
+  passwordForm: FormGroup;
+  confirmPasswordCtrl: FormControl;
+  loginCtrl: FormControl;
   birthYearCtrl: FormControl;
 
   constructor(fb: FormBuilder) {
-    this.passwordCtrl = fb.control('', [Validators.required]);
-    this.birthYearCtrl = fb.control('', Validators.required);
-    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)]);
+    this.passwordForm = fb.group({
+      password: (this.passwordCtrl = fb.control('', Validators.required)),
+      confirmPassword: (this.confirmPasswordCtrl = fb.control('', Validators.required))
+    });
 
     this.userForm = fb.group({
-      login: this.loginCtrl,
-      password: this.passwordCtrl,
-      birthYear: this.birthYearCtrl
+      login: (this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)])),
+      passwordForm: this.passwordForm,
+      birthYear: (this.birthYearCtrl = fb.control('', Validators.required))
     });
   }
 
