479c809d3f46c66f0bc64a8545415b6b17e9a018
Joe Romine <joe.romine@ufl.edu>
2021-11-21 15:50:17 -0800

    updates

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
@@ -8,21 +9,34 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegisterComponent {
   userForm: FormGroup;
+  passwordForm: FormGroup;
   passwordCtrl: FormControl;
-  loginCtrl: FormControl = new FormControl('', Validators.required);
+  confirmPasswordCtrl: FormControl;
+  loginCtrl: FormControl;
   birthYearCtrl: FormControl;
 
   constructor(fb: FormBuilder) {
     this.passwordCtrl = fb.control('', Validators.required);
-    this.birthYearCtrl = fb.control('', Validators.required);
-    this.loginCtrl = fb.control('', Validators.required);
+    this.birthYearCtrl = fb.control('', [Validators.required, Validators.min(1900),Validators.max(new Date().getFullYear())]);
+    this.loginCtrl = fb.control('', [Validators.required, Validators.minLength(3)]);
+    this.confirmPasswordCtrl = fb.control('', Validators.required);
+
+    this.passwordForm = fb.group({
+      // eslint-disable-next-line prettier/prettier
+      password: this.passwordCtrl = fb.control(''),
+      confirmPassword: this.confirmPasswordCtrl = fb.control('')
+    });
 
     this.userForm = fb.group({
       login: this.loginCtrl,
-      password: this.passwordCtrl,
+      password: this.passwordForm,
       birthYear: this.birthYearCtrl
     });
   }
 
   register() {}
+
+  static passwordMatch(passwordForm: FormGroup){
+   return (passwordForm.get('password')?.value == passwordForm.get('confirmPassword')?.value) ? {matchingError: null} : {matchingError: true};
+  }
 }
